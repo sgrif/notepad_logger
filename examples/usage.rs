f37332f64799a635b09cadd132d55b72c08acc52
Naim A <227396+naim94a@users.noreply.github.com>
2020-05-22 18:08:11 +0300

    add unicode support

--- examples/usage.rs
+++ examples/usage.rs
@@ -5,4 +5,5 @@ fn main() {
     log::set_max_level(LevelFilter::Info);
     info!("Hello!");
     info!("This should appear in your open notepad window");
+    warn!("It works with unicode too \u{1F642}\u{1F642}\u{1F642}");
 }
